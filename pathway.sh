1af6f8af883aa10529bb120bdcd340742699178b
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-03-05 18:22:49 -0500

    Update pathway.sh
    fix typo in wezterm name

--- pathway.sh
+++ pathway.sh
@@ -10,7 +10,7 @@ rm elvish-v0.20.1.tar.gz elvish-v0.20.1
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo apt update
-sudo apt install mezterm
+sudo apt install wezterm
 
 # set the config for wezterm
 cp wezterm.lua ~/.wezterm.lua
