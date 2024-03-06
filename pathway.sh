99672881da469604d69df9be7a9c46f5faaf7a54
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-03-06 18:24:33 -0500

    Update pathway.sh

--- pathway.sh
+++ pathway.sh
@@ -10,7 +10,7 @@ rm elvish-v0.20.1.tar.gz elvish-v0.20.1
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo apt update
-sudo apt install wezterm
+sudo apt install wezterm -Y
 
 # set the config for wezterm
 cp wezterm.lua ~/.wezterm.lua
@@ -21,3 +21,22 @@ cp rc.elv ~/.config/elvish/rc.elv
 
 # set elvish as the login shell for user specified (self)
 sudo usermod --shell /usr/local/bin/elvish $1
+
+###########  This section below is the dwarven-toolbox compiling and installing:
+
+# update cargo with rustup or install cargo with rustup if no rustup found
+rustup update || curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# git clone dwarven-toolbox from github
+git clone https://github.com/jpegleg/dwarven-toolbox || rm -rf dwarven-toolbox && git clone https://github.com/jpegleg/dwarven-toolbox
+cd dwarven-toolbox || exit 1
+
+# skip zlib compression tools by default
+mv no-zlib_Cargo.toml Cargo.toml
+
+# compile and install all 80 of the dwarven-toolbox programs
+cargo build --release --all
+for x in $(grep ^- README.md | grep -v 84 | cut -d'-' -f2); do
+    echo "Installing $x"
+    sudo cp target/release/$x /usr/local/bin/ 
+done
