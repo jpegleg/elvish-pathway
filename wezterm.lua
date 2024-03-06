9a83788484238ae8b200f4142235651e5324d97e
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-03-05 19:13:57 -0500

    Update wezterm.lua
    add background gradient and opacity to wezterm look

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,11 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = 'Mathias'
-
+config.window_background_opacity = 0.9
+config.window_background_gradient = {
+  colors = { '#000066', '000003' },
+  -- Specifices a Linear gradient starting in the top left corner.
+  orientation = { Linear = { angle = -45.0 } },
+}
 -- and finally, return the configuration to wezterm
 return config
