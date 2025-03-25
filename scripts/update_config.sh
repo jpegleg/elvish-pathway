b1b5626e98ed00d10bf09ceec65f966233ea8b2f
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-03-25 01:36:17 -0400

    Update update_config.sh
    change to use wormsign-confgen

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-sed -i "s|TEMPLATE|$1|g" "$3" &&
-sed -i "s|TPATH|$2|g" "$3" &&
-sleep 1 && # reduces chances of failed signs... try to optimize this
+cd "$5" &&
+sleep 0.2 &&
+/usr/local/bin/wormsign-confgen "$1" "$2" "$3" "$4" &&
+sleep 0.2 &&
 /usr/local/bin/wormsign -ats > sign.json &&
 /usr/local/bin/wormsign -v > verify.json
