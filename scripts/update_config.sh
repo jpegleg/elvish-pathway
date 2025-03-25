1d6e6071e1b24155bbf193a51d4a071c40791f81
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-03-25 00:02:24 -0400

    Update update_config.sh
    reducing chances of failure to sign in thread, likely a race condition happening - folding wormsign config generation into wormsign itself may be the real solution so we can avoid the sed usage

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-sed -i "s|TEMPLATE|$1|g" "$3"
-sed -i "s|TPATH|$2|g" "$3"
-/usr/local/bin/wormsign -ats > sign.json 2>/dev/null
-/usr/local/bin/wormsign -v > verify.json 2>/dev/null
+sed -i "s|TEMPLATE|$1|g" "$3" &&
+sed -i "s|TPATH|$2|g" "$3" &&
+sleep 1 && # reduces chances of failed signs... try to optimize this
+/usr/local/bin/wormsign -ats > sign.json &&
+/usr/local/bin/wormsign -v > verify.json
