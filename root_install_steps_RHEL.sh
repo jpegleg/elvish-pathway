5908190bbde280a4604e8bbbc62da5b8e9954bc0
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-06-12 21:55:56 -0400

    Update root_install_steps_RHEL.sh
    add gcc

--- root_install_steps_RHEL.sh
+++ root_install_steps_RHEL.sh
@@ -3,7 +3,7 @@
 curl -so - https://dl.elv.sh/linux-amd64/elvish-v0.21.0.tar.gz | sudo tar -xzvC /usr/local/bin
 
 sudo dnf install -y https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203_110809_5046fc22-1.fedora39.x86_64.rpm
-sudo dnf install -y pkgconf perl-FindBin perl-IPC-Cmd openssl-devel
+sudo dnf install -y pkgconf gcc perl-FindBin perl-IPC-Cmd openssl-devel
 
 # set elvish as the login shell for user specified, the user that be leveraging it
 sudo usermod --shell /usr/local/bin/elvish $1
